b6e51614d84d679e88af30dcbe6540f8892d1a2d
Barbara Perlik <barbperlik@gmail.com>
2017-11-01 19:30:25 -0400

    my stack passed testing

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -3,15 +3,26 @@ class MyStack
 
   def initialize
     @stack = Array.new
-    self.top = nil
+    @top = nil
+    @position = -1
   end
 
   def push(item)
+    @position += 1
+    @top = item
+    @stack[@position] = @top
   end
 
   def pop
+    raise "This stack is empty" if empty?
+    temp = @top
+    @stack[@position] = nil
+    @position -= 1
+    @top = @stack[@position]
+    return temp
   end
 
   def empty?
+    @stack.length<=0
   end
 end
